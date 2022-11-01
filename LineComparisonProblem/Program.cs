ffa9ee07e5d313d1f1c9443735caeb9f5dc2e2da
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-01 14:22:56 +0530

    UC1 Length of Line

---
+++ LineComparisonProblem/Program.cs
@@ -0,0 +1,26 @@
+﻿using LineComparisonProblem.Repository;
+
+namespace LineComparisonProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter X1 Co-ordinate : ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+           
+            Console.Write("Enter X2 Co-ordinate : ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter Y1 Co-ordinate : ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter Y2 Co-ordinate : ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+
+            LineComparision line = new LineComparision(x1, x2, y1, y2);
+            line.CalculateLengthOfLine();
+        }
+    }
+}
